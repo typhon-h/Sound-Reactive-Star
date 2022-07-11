6c21bfaab16cfd57fd7d15000976108ae0dcec51
Harrison Tyson <hty13@uclive.ac.nz>
2022-07-11 13:18:57 +1200

    Added IR to scheduler

--- suda_led_feature.ino
+++ suda_led_feature.ino
@@ -1,11 +1,21 @@
+#include "ir.h"
+
 // Task Scheduler
 #include <TaskScheduler.h>
 #include <TaskSchedulerDeclarations.h>
 Scheduler schedule;
 
+// Initialize tasks
+#define IR_TASK_FREQUENCY 400
+Task ir_task(IR_TASK_FREQUENCY, TASK_FOREVER, &ir_poll);
+
 void setup()
 {
+    ir_setup();
     schedule.init();
+    schedule.addTask(ir_task);
+
+    ir_task.enable();
 }
 
 void loop()
