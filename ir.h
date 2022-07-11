0247c05152dbcc43e84d3a7993e604e438f9af3c
Harrison Tyson <hty13@uclive.ac.nz>
2022-07-11 12:58:18 +1200

    Added IR Module

---
+++ ir.h
@@ -0,0 +1,10 @@
+#ifndef IR_H
+#define IR_H
+#include <IRremote.hpp>
+#define IR_RECEIVE_PIN 11 // TODO: set pin
+
+void ir_setup(void);
+void ir_poll(void);
+void ir_run_command(int command);
+void enable(void);
+#endif
