8b964289597b0d0eb7f21435e66d17756136218c
Harrison Tyson <hty13@uclive.ac.nz>
2024-04-20 03:49:48 +1200

    Add low pass filter for aliasing

--- microphone.h
+++ microphone.h
@@ -7,7 +7,10 @@
 #define MIC_H
 
 #define LOG_OUT 1 // LOG output
-#define FHT_N 128 // 128 samples
+#define FHT_N 64  // 128 samples
+
+#define SAMPLE_RATE 9600   // Max ADC sample rate (Hz)
+#define LOW_PASS_FREQ 4000 // Cutoff frequency of the low-pass filter (Hz)
 
 #include <FHT.h> // FFT-like library
 
--- microphone.ino
+++ microphone.ino
@@ -11,15 +11,28 @@
 
 void microphone_setup(void)
 {
-    // Do something
+}
+
+int readAudioWithFilter()
+{
+    static float lastValue = 0;                                           // Previous filtered value
+    float alpha = LOW_PASS_FREQ / (LOW_PASS_FREQ + SAMPLE_RATE * 2 * PI); // Filter coefficient
+
+    // Read audio sample from microphone
+    int rawValue = analogRead(MICROPHONE_PIN);
+    float newValue = rawValue * (1 - alpha) + lastValue * alpha; // Low-pass filtering
+
+    lastValue = newValue; // Update last filtered value
+
+    return newValue; // Return filtered audio sample
 }
 
 void microphone_sample()
 {
+    cli();
     for (int i = 0; i < FHT_N; i++)
     {
-        int k = analogRead(MICROPHONE_PIN);
-        // Serial.println(k);
+        int k = readAudioWithFilter();
         fht_input[i] = k; // fill out array
     }
 
@@ -27,17 +40,12 @@ void microphone_sample()
     fht_reorder(); // reorder the data before doing the fht
     fht_run();     // process the data in the fht
     fht_mag_log(); // take the output of the fht
-
-    // 8 bins to break up the frequencies - map them to 4 strips
-    for (int i = 2; i < FHT_N / 2; i++)
-    {
-        if (fht_log_out[i] < 32)
-        {
-            fht_log_out[i] = 0;
-        }
-        else
-        {
-            fht_log_out[i] -= 32;
-        }
-    }
+    sei();
+    // For Microphone level visualizer
+    // for (int i = 0; i < (FHT_N / 2) - 1; i++)
+    // {
+    //     Serial.print(fht_log_out[i]);
+    //     Serial.print(",");
+    // }
+    // Serial.println(fht_log_out[(FHT_N / 2) - 1]);
 }
