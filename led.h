3a0fc095292c31429f8ea968f52d3ec1e42dbfa5
Harrison Tyson <hty13@uclive.ac.nz>
2024-06-23 15:00:53 +1200

    Change default color

--- led.h
+++ led.h
@@ -39,7 +39,7 @@ typedef enum
 
 CRGB leds[NUM_STRIPS][LEDS_PER_STRIP];
 
-static uint8_t current_hue = 0;
+static uint8_t current_hue = 119;
 static EFFECT_T active_effect = PULSE;
 
 // Initialize FASTLED controller
